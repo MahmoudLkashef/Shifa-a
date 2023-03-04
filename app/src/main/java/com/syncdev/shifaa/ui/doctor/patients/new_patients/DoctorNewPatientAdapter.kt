a20b23c7282073dccae8abf4a661996f320adc76
Muhammed <muhammed.hassaann@gmail.com>
2023-03-04 02:00:45 +0200

    Finished creating the doctor patient layouts

---
+++ app/src/main/java/com/syncdev/shifaa/ui/doctor/patients/new_patients/DoctorNewPatientAdapter.kt
@@ -0,0 +1,54 @@
+package com.syncdev.shifaa.ui.doctor.patients.new_patients
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.syncdev.shifaa.databinding.NewPatientRequestListItemBinding
+import com.syncdev.shifaa.ui.doctor.patients.past_patients.PatientTest
+
+class DoctorNewPatientAdapter :
+    ListAdapter<PatientTest, DoctorNewPatientAdapter.DoctorNewPatientViewHolder>(
+        DiffCallback()
+    ) {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DoctorNewPatientViewHolder {
+        val binding = NewPatientRequestListItemBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return DoctorNewPatientViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: DoctorNewPatientViewHolder, position: Int) {
+        val item = getItem(position)
+        holder.bind(item)
+    }
+
+    class DoctorNewPatientViewHolder(private val binding: NewPatientRequestListItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: PatientTest) {
+            binding.apply {
+                tvNewPatientName.text = item.name
+                tvDateNewPatient.text = item.date
+                tvNewPatientTime.text = item.time
+                imgNewPatientPic.setImageResource(item.profilePic)
+            }
+        }
+    }
+
+}
+
+
+class DiffCallback : DiffUtil.ItemCallback<PatientTest>() {
+    override fun areItemsTheSame(oldItem: PatientTest, newItem: PatientTest): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: PatientTest, newItem: PatientTest): Boolean {
+        return oldItem == newItem
+    }
+}
--- app/src/main/java/com/syncdev/shifaa/ui/doctor/patients/past_patients/DoctorPastPatientsFragment.kt
+++ app/src/main/java/com/syncdev/shifaa/ui/doctor/patients/past_patients/DoctorPastPatientsFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.GridLayoutManager
 import com.syncdev.shifaa.R
 import com.syncdev.shifaa.databinding.FragmentDoctorPastPatientsBinding
 
@@ -12,13 +14,45 @@ import com.syncdev.shifaa.databinding.FragmentDoctorPastPatientsBinding
 class DoctorPastPatientsFragment : Fragment() {
 
     private lateinit var binding: FragmentDoctorPastPatientsBinding
+    private lateinit var adapter: DoctorPastPatientsAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = FragmentDoctorPastPatientsBinding.inflate(inflater,container,false)
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_doctor_past_patients,
+            container,
+            false
+        )
+
+        adapter = DoctorPastPatientsAdapter()
+        val pastPatientsList = listOf<PatientTest>(
+            PatientTest(1,"Mohammed Magdy","22 Feb 2023","10:00 AM",R.drawable.patient_male),
+            PatientTest(2,"Mohammed Ayman","29 Feb 2023","10:30 AM",R.drawable.patient_male),
+            PatientTest(3,"Sarah Magdy","25 Jan 2023","11:00 AM",R.drawable.patient_female),
+            PatientTest(4,"Abdulmageed Awad","22 Oct 2022","11:30 AM",R.drawable.patient_male),
+            PatientTest(5,"Hossam Khedr","15 Feb 2023","12:00 PM",R.drawable.patient_male),
+            PatientTest(6,"Kholoud Rady","6 Feb 2023","12:30 PM",R.drawable.patient_female),
+            PatientTest(7,"Samy Ahmed","13 Dec 2022","1:00 PM",R.drawable.patient_male),
+            PatientTest(8,"Khaled Omar","26 Jan 2023","1:30 PM",R.drawable.patient_male),
+            PatientTest(9,"Aya Sayed","3 Mar 2023","2:00 PM",R.drawable.patient_female),
+            PatientTest(10,"Mohab Hussein","1 Mar 2023","2:30 PM",R.drawable.patient_male),
+            PatientTest(11,"Walaa Ali","11 Nov 2022","3:30 PM",R.drawable.patient_female),
+            PatientTest(12,"Lila Salah","8 Feb 2023","4:00 PM",R.drawable.patient_female),
+            PatientTest(13,"Mahmoud Reda","3 Feb 2023","4:30 PM",R.drawable.patient_male),
+            PatientTest(14,"Mohammed Hassaan","3 Feb 2023","5:00 PM",R.drawable.patient_male),
+        )
+
+        binding.rvPastPatients.layoutManager = GridLayoutManager(requireContext(),2)
+
+        binding.tvPastPatientsNumber.text = "${pastPatientsList.size} Old patients"
+
+        adapter.submitList(pastPatientsList)
+
+        binding.rvPastPatients.adapter = adapter
 
 
         return binding.root
