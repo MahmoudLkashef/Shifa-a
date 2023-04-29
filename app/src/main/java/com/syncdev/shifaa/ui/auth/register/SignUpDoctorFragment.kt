a6034a9819d151602637f7820bbbbe7cb3d26c76
mahmoudLkashef <mahmoud.reda.e12@gmail.com>
2023-04-29 05:35:03 +0300

    Added doctor data class and setup ui logic

--- app/src/main/java/com/syncdev/shifaa/ui/auth/register/SignUpDoctorFragment.kt
+++ app/src/main/java/com/syncdev/shifaa/ui/auth/register/SignUpDoctorFragment.kt
@@ -1,19 +1,23 @@
 package com.syncdev.shifaa.ui.auth.register
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.RadioButton
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
+import com.syncdev.domain.model.Doctor
 import com.syncdev.shifaa.R
 import com.syncdev.shifaa.databinding.FragmentSignUpDoctorBinding
 
 
 class SignUpDoctorFragment : Fragment() {
 
-
+    private val TAG="SignUpDoctorFragment"
     private lateinit var binding: FragmentSignUpDoctorBinding
 
     override fun onCreateView(
@@ -33,10 +37,46 @@ class SignUpDoctorFragment : Fragment() {
         }
 
         binding.btnDoctorSignup.setOnClickListener {
+
+            val doctorData=getDoctorData()
+            val password=binding.etDoctorPassword.text.toString()
+            val confirmPassword=binding.etDoctorConfirmPassword.text.toString()
+            if(password == confirmPassword) {
+                // call UseCase
+                Log.i(TAG, ""+doctorData)
+            }
+
             findNavController().navigate(SignUpDoctorFragmentDirections.actionSignUpDoctorFragmentToSignInFragment())
         }
 
         return binding.root
+    }
+
+    private fun getDoctorData():Doctor{
+        val firstName =binding.etFnameDoctor.text.toString()
+        val lastName=binding.etLnameDoctor.text.toString()
+        val gender=getDoctorGender()
+        val speciality=binding.dropdownMenuDoctorSpecialty.text.toString()
+        val phoneNumber=binding.etDoctorMobileNumber.text.toString()
+        val email=binding.etDoctorEmail.text.toString()
+
+        return Doctor(
+            firstName=firstName,
+            lastName = lastName,
+            gender = gender,
+            phoneNumber = phoneNumber,
+            email = email,
+            speciality = speciality
+        )
+    }
+    private fun getDoctorGender():String{
+        val selectedId = binding.radoiGroupDoctorGender.checkedRadioButtonId
+
+        return if (selectedId != -1) {
+            val selectedRadioButton = binding.root.findViewById<RadioButton>(selectedId)
+            val selectedGender = selectedRadioButton.text.toString()
+            selectedGender
+        } else ""
     }
 
 
