d111538b1ea622d5fefd4c904375283ee666b6cd
mahmoudLkashef <mahmoud.reda.e12@gmail.com>
2023-06-12 04:08:40 +0300

    Created prescription reports list item

--- app/src/main/java/com/syncdev/shifaa/ui/patient/reports/PatientReportsFragment.kt
+++ app/src/main/java/com/syncdev/shifaa/ui/patient/reports/PatientReportsFragment.kt
@@ -5,18 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.syncdev.shifaa.R
+import com.syncdev.shifaa.databinding.FragmentPatientReportsBinding
 
 
 class PatientReportsFragment : Fragment() {
-
-
+    private val TAG="PatientReportsFragment"
+    private lateinit var binding:FragmentPatientReportsBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_patient_reports, container, false)
+        binding = DataBindingUtil.inflate(
+            layoutInflater,
+            R.layout.fragment_patient_reports,
+            container,
+            false
+        )
+
+        return binding.root
     }
 
 
