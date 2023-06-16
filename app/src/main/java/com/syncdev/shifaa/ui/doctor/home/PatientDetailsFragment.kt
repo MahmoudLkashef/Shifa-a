99d8cc7de473dc00945128a629dee057e4cee4de
mahmoudLkashef <mahmoud.reda.e12@gmail.com>
2023-06-16 19:43:49 +0300

    Added back button functionality and changed input type of frequency

--- app/src/main/java/com/syncdev/shifaa/ui/doctor/home/PatientDetailsFragment.kt
+++ app/src/main/java/com/syncdev/shifaa/ui/doctor/home/PatientDetailsFragment.kt
@@ -55,6 +55,10 @@ class PatientDetailsFragment : Fragment() {
                 Dialogs().addNewMedicineToPrescriptionDialog(requireContext(),prescriptionViewModel)
 
             }
+
+            btnBackPatientDetails.setOnClickListener {
+                findNavController().popBackStack()
+            }
         }
 
 
